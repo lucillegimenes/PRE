20b5809a817eb5e0eb13019028ee5a0528407250
lucillegimenes <62749866+lucillegimenes@users.noreply.github.com>
2021-08-14 15:50:29 +0200

    Add files via upload

---
+++ D_G_main.m
@@ -0,0 +1,241 @@
+%This code simulates runs for STEADY or SEASONAL environment 
+%Things need to be defined beforehand : 
+% - Number of size classes Imax, number of trophic strategy classes Jmax
+% - steady (seasonal=0) or seasonal (seasonal=1) environmental conditions
+% - run time in terms or years 
+
+%Structure of the code
+% 1. define state variables
+% 2. define environmental conditions
+% 3. solve the equations with ODE
+% 4. use that output for calculation of interest variables (diversity,
+% trophic transfer, etc)
+% 5. plot the figures needed
+
+ %==== Dialog box ====%
+rep = inputdlg({'Size classes (number)','Trait classes (number)','seasonal (0/1)','years (number)'},'Requirements',[1 12;1 12;1 12;1 12]);
+
+%The following global variables are needed for the ODE function
+global Imax Jmax x xo y 
+global alphaF palat mp0 m2 Z 
+global gmax P0 reminN reminS D0
+global JR betaL betaN betaSi betaF
+global L N0 S0 dil 
+
+
+%==== Parameters ====%
+%Carbon,Nitrogen,Si density
+cb = 0.11e-6; % [µgC/µm^3] carbon content of cytoplasm (from Sthratmann,1967)
+cm = 0.6e-6;  % [µgC/µm^3] carbon content of membranes
+gb = 7;       % C:N ratio of cytoplasm [µgC/µgN]
+gm = 38;      % C:N ratio of membrane [µgC/µgN]
+Sishell = 10.3E-6; % silicate concentration in diatom shell [µg Si µm-3]
+%cL = 0.0318*(gb/cb)^(-2/3);   % light affinity coefficient [µgC/day/(W m^-2)/µm^2]
+
+%Thickness
+h = 0.008;    % cell membranes [µm]
+tshell = 0.01;     % shell [µm] 
+
+%Affinity coefficients
+kappa = 0.2;     % light affinity investment return [µm^-1] (also called absorption coefficient)
+%cN = 2.89E-4*(gb/cb)^(-1/3); % nutrient affinity coefficient [L/day/µm]
+%cP = 0.227*(gb)^(-1); % food affinity coefficient [L/day/µgC]
+%eP = 0.3408*(gb)^(-1) ; % nutrient affinity investment return [L/day/µgC]
+
+alphaN = 0.68;  % L day-1 (µg C)-1 µm2  
+alphaL = 0.18 * 1E-12 * 24 * 3600*1.875;   % quantum yield (scaled)[µgC day-1 (µE m-2 s-1)-1 µm-2] 
+qyield = 0.07; % quantum yield {µg C / µmol photons PAR]
+%useful for the calculation of nutrients affinity
+rstar = 2;  % crossover from r3 tp r absolute nutrient affinity 
+xstar = 4*pi/3*rstar^3*cb; % mass equivalence for rstar
+
+%Food 
+alphaF = 0.018; %0.018 food affinity coefficient[L d-1 (µgC)-1]
+palat = 0.6; % diatom palatibility factor
+epsilon = 7; % predator-prey size ratio (linear dimension)
+width = 1; % in orders of magnitude
+mp0 = 0.1; % grazing constant 
+m2 = 0.01; % quadratic viral lysis [(µC L^-1)^-1 d^-1]
+gmax = 1; %maximal growth rate [d^-1]
+Z0 = 0.06; %copepods [µgC L^-1] (predation by higher trophic levels)
+reminS = 0.8; reminN = 0.1; %remineralization rates 
+
+%Metabolic costs 
+JR = 0.04; % basal respiration [µgC µgC^-1 day^-1] (beta0)
+betaL = 0.08;   % cost of light harvesting  [µgC µgC^-1]
+betaN = 0.45;   % cost of nitrogen uptake  [µgC µgN^-1]
+betaSi = 0.45;  % cost of silicate uptake  [µgC µgSi^-1]
+betaF = 0.45;   % cost of particulate matter uptake  [µgC µgC^-1]
+
+%Classes
+Imax = str2num(rep{1}); %Number of size classes 
+Jmax = str2num(rep{2}); %Number of classes for vacuolation or trophic strategies 
+
+
+%==== Primary trait space ====%
+xo = 10.^linspace(-7,1,Imax); %cell carbon mass ranging from 10^-7 to 10 µgC cell^-1
+[x,y] = meshgrid(xo,linspace(0,1,Jmax)); %y is phagotrophic feeding for generalists
+vo = linspace(0,1,Jmax+1); % vacuole size for diatoms 
+[x,v] = meshgrid(xo,vo(1:Jmax));
+r = (3/(4*pi*cb)*x./(1-v)).^(1/3); %celle equivalent spherical radius [µm]
+
+phib = 0; %Investment in biomass photosynthesis
+xm = 4*pi*r.^2*h*cm.*(1 + v.^(2/3)); %Carbon mass in membranes
+phim = xm./x; %Carbon amount apportioned to membrane mass
+
+%Generalists
+G.x = x; G.r = ones(Jmax,1)*r(1,:); %(only radius with v=0)
+G.phim = ones(Jmax,1)*phim(1,:); %phi_M(x,0) (first line of phim corresponds to v=0) 
+phil = 1 - phim - phib - y; phil(phil<0) = 0; 
+phif = y; phif(phil<0) = 0;
+G.phil = phil; G.phif = phif;
+
+%Diatoms
+D.x=x;D.v=v;D.r=r; 
+D.phim=phim;
+D.phim(D.phim > 1) = 1;
+phil = 1 - phim - phib; phil(phil<0) = 0;
+D.phil = phil;
+
+%N and Si requirements
+D.shell = 2.28E-3*(D.r).^(0.72); % thickness of diatom shell (t=1.62V^0.24)
+D.s = 4*pi*Sishell*(D.r.^2).*D.shell; % diatom Si requirement [µg Si]
+D.n = xm/gm + (D.x - D.phim.*D.x)/gb; % diatom N requirement [µg N]
+%D.n(D.n<0)=0;
+G.n = G.phim.*G.x/gm + (G.x-G.phim.*G.x)/gb; %generalist N requirement [µg N]
+
+%C:Si ratio
+D.CS = D.x./D.s; 
+%C:N ratio 
+D.CN= D.x./D.n;
+G.CN = G.x./G.n; 
+
+%==== Affinitites ====%
+D.AL = alphaL*pi*(D.r.^2).*(1 - exp(-kappa*D.phil.*D.r.*(1-D.v)))./D.x; D.AL = D.AL*0.9; %0.9 factor is an attempt of correction to match Mathilde's value
+D.AN = 4/3*pi*cb*alphaN*(D.r)./(1 + (D.x/xstar).^(-2/3))./D.x; D.AN = D.AN;
+D.AN(D.phim == 1) = 0;
+
+G.AL = alphaL*pi*(G.r.^2).*(1 - exp(-kappa*G.phil.*G.r))./G.x;  
+G.AN = alphaN*((G.r).^(-2))./(1 + (G.r/rstar).^(-2)); 
+G.AF = alphaF*G.phif; 
+
+G.JF = zeros(size(G.AF));
+
+%==== Trophic interactions ====%
+Theta = @(rpredator,rprey) exp(-(log(epsilon*rprey./rpredator)).^2/(2*width^2)); %represents size preference for smaller cells 
+
+T = zeros(Jmax,Imax,Imax);  
+for i = 1:Imax  % calculated once 
+    T(:,:,i) = Theta(r(1,i),D.r); %3D matrix with all possible predator prey interactions 
+end
+
+%==== Time parameters ====%
+tf = 365;
+tspan=[1:tf];
+years = str2num(rep{4});
+tspan_y = [1:tf*years];
+
+%==== Steady or seasonal environmental forcing ====%
+seasonal = str2num(rep{3});
+
+%==== Initial conditions ====%
+%Plankton
+Binit = 1;  % Initial total biomass abundance [mgC m^-3]
+BGtoD = 0.7;  % biomass split BGtoD in generalists, 1- BGtoD in diatoms
+P0 = ones([Jmax Imax])*0.09;% %Background plankton biomass concentration [mgC L^-1] 
+D0 = P0; %backroung concentration for diatoms 
+
+%little adjusment
+for i=1:Imax*Jmax
+    if D.AN(i)==0
+        D0(i)=0; %taking out the "small cell size, big vacuole" diatoms that should'nt exist
+    end 
+end
+
+D.b = ones(size(phil))*Binit*(1-BGtoD)./(Imax*Jmax); %initial total biomass abundance for diatoms 
+G.b = ones(size(phil))*Binit* BGtoD./(Imax*Jmax); %initial total biomass abundance for generalists
+
+%correct format for the ODE solver
+B0 = [reshape(D.b,[Jmax*Imax 1]);reshape(G.b,[Jmax*Imax 1])]; %initial vector for the ODE
+maxind = 2*Imax*Jmax; %size of the initial vector
+
+%Environmental conditions
+lat = 55; %possible to change latitude
+[L,dil,Z,N0,S0]=D_G_physical_settings(lat,years,seasonal,tspan,tf); 
+
+%==== Solving the equations ====%
+if seasonal ==0
+    D.allB = []; G.allB = D.allB; 
+    D.Bio = zeros(tf*years,Jmax,Imax); G.Bio = D.Bio;
+    Ls = L; dils= dil; Zs = Z; N0s = N0; S0s= S0; 
+    for s=1:3 %for the 3 steady states
+        L=Ls(s); dil = dils(s); Z = Zs(s); N0=N0s(s); S0 = S0s(s);
+        
+         options = odeset('NonNegative',[1:maxind],'RelTol',1e-4,'AbsTol',1e-4); 
+        [t, B] = ode45(@(t,B) D_G_ode_func(t,B,D,G,T,seasonal),tspan_y,B0,options);
+        [No_use,HD,HG] = D_G_ode_func(t,B,D,G,T,seasonal)  ; %output of D and G
+        for it=1:length(t)
+            %reshaping biomass into a 3D matrix
+            D.Bio(it,:,:) = reshape(squeeze(B(it,1:Jmax*Imax)),[Jmax Imax]); 
+            G.Bio(it,:,:) = reshape(squeeze(B(it,Jmax*Imax+1:Jmax*2*Imax)),[Jmax Imax]);
+        end 
+        D.allB = [D.allB; D.Bio]; G.allB =[G.allB; G.Bio]; %Biomass for the 3 steady states 
+    end 
+
+else 
+    B0 = [B0;N0;S0]; %if seasonal, nutrients concentration also varies over time -> (2*Jmax*Imax + 2) vector : nutrients variables at the end 
+    maxind = Jmax*2*Imax+2; %size of the vector
+    
+    options = odeset('NonNegative',[1:maxind],'RelTol',1e-4,'AbsTol',1e-4); 
+    [t, B] = ode45(@(t,B) D_G_ode_func(t,B,D,G,T,seasonal),tspan_y,B0,options);
+    [No_use,HD,HG] = D_G_ode_func(t,B,D,G,T,seasonal)  ; %output of D and G
+
+    D.Bio = zeros(tf*years,Jmax,Imax); G.Bio = D.Bio; G.photo=G.Bio; G.mixo=G.photo;
+    for it=1:length(t)
+        %reshaping biomass into a 3D matrix
+        D.Bio(it,:,:) = reshape(squeeze(B(it,1:Jmax*Imax)),[Jmax Imax]); 
+        G.Bio(it,:,:) = reshape(squeeze(B(it,Jmax*Imax+1:Jmax*2*Imax)),[Jmax Imax]);
+        G.photo(it,:,:) = squeeze(G.Bio(it,:,:)).*G.phil;
+        G.mixo(it,:,:) = squeeze(G.Bio(it,:,:)).*(ones(Jmax,Imax)-G.phil);
+    end
+
+    %nutrients concentration
+    N = B(:,2*Jmax*Imax+1);
+    Si = B(:,2*Jmax*Imax+2);
+    
+    %diversity indexes
+    [D.Simpson, G.Simpson, D.Shannon,G.Shannon, Totsimpson, Totshannon]=D_G_diversity(D,G,t,tf,years);
+
+    
+end   
+
+
+    D.Btot = squeeze(sum(D.Bio,2)); %sum for all vacuoles sizes for one size class
+    G.Btot = squeeze(sum(G.Bio,2)); %sum for all trophic strategies for one size class
+    D.Btotot = sum(D.Btot,2); %total biomass for diatoms at each time step (size = [years*365 1])
+    G.Btotot = sum(G.Btot,2);
+    G.photo_tot = sum(squeeze(sum(G.photo,2)),2);
+    G.mixo_tot = sum(squeeze(sum(G.mixo,2)),2);    
+    
+%==== Trophic transfer ====%
+    if seasonal==1
+        [TTE_2, TTE_3, TTE_np,divmixo,divphoto]= D_G_extra_output(t,D,G,T,N,Si,tf,years);
+        
+                %==== Productivity ====%
+        D.prod = zeros(years*tf,Jmax,Imax);
+        G.prod = zeros(years*tf,Jmax,Imax);
+        for it=1:years*tf
+            D.prod(it,:,:)=L(it)*D.AL.*reshape(D.Bio(it,:,:),[Jmax Imax]);
+            G.prod(it,:,:)=L(it)*G.AL.*reshape(G.Bio(it,:,:),[Jmax Imax]);
+        end
+
+        D.prod = sum(squeeze(sum(D.prod,2)),2)*10^(-3);
+        G.prod = sum(squeeze(sum(G.prod,2)),2)*10^(-3);
+
+    end 
+
+%==== Plot ====%
+
+D_G_plot; 
+
+
---
+++ D_G_plot.m
@@ -0,0 +1,416 @@
+%This script plot all the necessary figures, for steady or seasonal simulation:
+%Dialog box asks if you're interest by diversity plot and if so which diversity index
+%would you like
+toppanelpos = [.13,.51,.78,.4];
+botpanelpos = [.13,.09,.78,.4];
+
+%for axes display
+pow = char('10^{-7}','10^{-5}','10^{-3}','10^{-1}','10');
+power = char('10^{-7}','10^{-6}','10^{-5}','10^{-4}','10^{-3}','10^{-2}','10^{-1}','1','10');
+
+%==== Steady state ====% 
+if seasonal==0 
+    pos1= [.06,.66,.27,.27]; pos2= [.36,.66,.27,.27]; pos3= [.66,.66,.27,.27];
+    pos4= [.06,.36,.27,.27]; pos5= [.36,.36,.27,.27]; pos6= [.66,.36,.27,.27];
+    pos7= [.06,.06,.27,.27]; pos8= [.36,.06,.27,.27]; pos9= [.66,.06,.27,.27];
+  
+    figure(20)
+    clf;
+    set(gcf,'color','w');
+    subplot('Position',pos1)
+    D.Btot = squeeze(sum(D.allB(1:365,:,:),2)); %sum for all vacuoles sizes for one size class
+    G.Btot = squeeze(sum(G.allB(1:365,:,:),2));
+    D.Btotot = sum(D.Btot(end,:),2); G.Btotot = sum(G.Btot(end,:),2);
+    plot(log10(xo),D.Btot(end,:),log10(xo),G.Btot(end,:)); 
+    legend('Diatoms','Generalists');
+    title('Winter'); ylabel('Biomass [µgC L^{-1}]');
+    xlim([-7 1]);xticks([-7 -5 -3 -1 1]); xticklabels(pow); 
+
+    
+    subplot('Position',pos4)
+    futG=reshape(G.allB(365,:,:),[Jmax Imax]);
+    imagesc(log10(xo),1-y(:,1),futG);
+    set(gca, 'YDir', 'normal');colormap(jet(50)); c=colorbar;
+    ylabel(c, 'P \rm [µgC L^{-1} ]')
+    ylabel('\rm \phi_L'); set(gca,'XTick', []);
+   
+    
+    subplot('Position',pos7)
+    futD=reshape(D.allB(365,:,:),[Jmax Imax]);
+    imagesc(log10(xo),D.v(:,1),futD);
+    set(gca, 'YDir', 'normal');colormap(jet(50)); c=colorbar;
+    ylabel('\rm v'); xlabel('\it x \rm [µg C]'); xticks([-7 -5 -3 -1 1]); xticklabels(pow);
+    
+    subplot('Position',pos2)
+    D.Btot = squeeze(sum(D.allB(366:730,:,:),2)); %sum for all vacuoles sizes for one size class
+    G.Btot = squeeze(sum(G.allB(366:730,:,:),2));
+    D.Btotot = [D.Btotot sum(D.Btot(end,:),2)]; G.Btotot =[G.Btotot sum(G.Btot(end,:),2)]; 
+    plot(log10(xo),D.Btot(end,:),log10(xo),G.Btot(end,:)); title('Spring');xlim([-7 1]); xticks([-7 -5 -3 -1 1]); xticklabels(pow);
+    
+    subplot('Position',pos5)
+    futG=reshape(G.allB(730,:,:),[Jmax Imax]);
+    imagesc(log10(xo),1-y(:,1),futG);
+    set(gca, 'YDir', 'normal');colormap(jet(50)); c=colorbar;
+    axis off
+    
+    subplot('Position',pos8)
+    futD=reshape(D.allB(730,:,:),[Jmax Imax]);
+    imagesc(log10(xo),D.v(:,1),futD);
+    set(gca, 'YDir', 'normal');colormap(jet(50)); c=colorbar;
+    xlabel('\it x \rm [µg C]'); xticks([-7 -5 -3 -1 1]); xticklabels(pow); set(gca,'YTick', []);
+   
+    
+    subplot('Position',pos3)
+    D.Btot = squeeze(sum(D.allB(731:1095,:,:),2)); %sum for all vacuoles sizes for one size class
+    G.Btot = squeeze(sum(G.allB(731:1095,:,:),2));
+    D.Btotot = [D.Btotot sum(D.Btot(end,:),2)]; G.Btotot =[G.Btotot sum(G.Btot(end,:),2)]; 
+    plot(log10(xo),D.Btot(end,:),log10(xo),G.Btot(end,:)); title('Summer');xlim([-7 1]); xticks([-7 -5 -3 -1 1]); xticklabels(pow);
+    
+    subplot('Position',pos6)
+    futG=reshape(G.allB(1095,:,:),[Jmax Imax]);
+    imagesc(log10(xo),1-y(:,1),futG);
+    set(gca, 'YDir', 'normal');colormap(jet(50)); c=colorbar;
+    axis off
+    
+    subplot('Position',pos9)
+    futD=reshape(D.allB(1095,:,:),[Jmax Imax]);
+    imagesc(log10(xo),D.v(:,1),futD);
+    set(gca, 'YDir', 'normal');colormap(jet(50)); c=colorbar;
+    xlabel('\it x \rm [µg C]'); xticks([-7 -5 -3 -1 1]); xticklabels(pow); set(gca,'YTick', []);
+   
+% 
+%     figure(2)
+%     clf;
+%     set(gcf,'color','w');
+%     plot(D.allSimp,D.Btotot,G.allSimp,G.Btotot); 
+%     xlabel('D'); ylabel('Total biomass [mgC m^{-3}]');
+%     legend('diatoms','generalists')
+%   
+    
+    
+%==== Seasonal cycle ====%    
+else 
+    pos1=[.06,.06,.28,.8] ; pos2=[.38,.06,.28,.8]  ; pos3= [.7,.06,.28,.8] ; 
+    
+    %Dialog box 
+    prompt ={'Do you want the diversity index ? (yes/no)','Which one ? (Simpson/Shannon'} ; 
+    dlgtitle= 'Diversity';
+    dims = [1 40;1 40];
+    answer = inputdlg(prompt,dlgtitle,dims);
+    user_v = string(answer{1});
+    user_w = string(answer{2});
+    
+    D.Btot_y=D.Btot((years-1)*tf+1:years*tf,:);
+    G.Btot_y=G.Btot((years-1)*tf+1:years*tf,:);
+    Bmax=max(max(max(G.Btot_y)),max(max(D.Btot_y)));
+    
+    %Biomass evolution for diatoms
+    figure(1)
+    set(gcf,'color','w');
+    ie = imagesc(t((years-1)*tf+1:years*tf),log10(xo),D.Btot_y',[0 Bmax]); 
+    set(gca, 'YDir', 'normal');
+    xlim([(years-1)*tf+1 years*tf])
+    monthstr = {'J','F','M','A','M','J','J','A','S','O','N','D'};
+    monthnr  = [  1   32  60  91  121 152 182 213 244 274 305 335]+ones(1,12)*tf*(years-1);
+    xticks(monthnr); %x axis : time in months
+    xticklabels(monthstr);
+    A=jet(120);
+    A(1,:)=1;
+    colormap(A);
+    C=colorbar;
+    xlabel('Months')
+    xlabel(C,'Biomass \rm [µgC L^{-1}]');
+    ylabel('Mass \rm [µgC]');
+    set(gca,'FontSize',14);
+    shading interp   
+    yticks([-7 -6 -5 -4 -3 -2 -1 0 1]); yticklabels(power); 
+
+     %Biomass evolution for generalists
+    figure(2)
+    set(gcf,'color','w');
+    ie = imagesc(t((years-1)*tf+1:years*tf),log10(xo),G.Btot_y',[0 Bmax]); 
+    set(gca, 'YDir', 'normal');
+    xlim([(years-1)*tf+1 years*tf])
+    monthstr = {'J','F','M','A','M','J','J','A','S','O','N','D'};
+    monthnr  = [  1   32  60  91  121 152 182 213 244 274 305 335]+ones(1,12)*tf*(years-1);
+    xticks(monthnr); %x axis : time in months
+    xticklabels(monthstr);
+    colormap(A);
+    C=colorbar;
+    xlabel('Months')
+    xlabel(C,'Biomass \rm [µgC L^{-1}]');
+    ylabel('Mass \rm [µgC]');
+    set(gca,'FontSize',14);
+    shading interp   
+    yticks([-7 -6 -5 -4 -3 -2 -1 0 1]); yticklabels(power); 
+    
+    %Evolution of nutrients concentration
+    figure(3)
+    set(gcf,'color','w');
+    plot(t((years-1)*tf+1:years*tf),N((years-1)*tf+1:years*tf),'b',t((years-1)*tf+1:years*tf),Si((years-1)*tf+1:years*tf),'g','LineWidth',2)
+    legend('N \rm [µgN L^{-1}]','Si \rm [µgS L^{-1}]');
+    xlim([(years-1)*tf+1 years*tf]);   
+    monthstr = {'J','F','M','A','M','J','J','A','S','O','N','D'};
+    monthnr  = [  1   32  60  91  121 152 182 213 244 274 305 335]+ones(1,12)*tf*(years-1);
+    xticks(monthnr) 
+    xticklabels(monthstr)
+    xlabel('Months')
+    
+
+    
+    
+%plotting dominant traits 
+    D.Btrait = zeros(tf*years,Imax);
+    for it=1:tf*years
+        for i=1:Imax
+            k=max(D.Bio(it,:,i));
+            kbis= max(find(D.Bio(it,:,i)==k));
+            D.Btrait(it,i) = D.v(kbis,1);
+            if D.Btot(it,i)< (Bmax/120) %below, biomass considered non-existent
+                D.Btrait(it,i) = NaN;
+            end
+        end 
+    end 
+
+    G.Btrait = zeros(tf*years,Imax);
+    for it=1:tf*years
+        for i=1:Imax
+            k=max(G.Bio(it,:,i));
+            kbis= max(find(G.Bio(it,:,i)==k));
+            G.Btrait(it,i) = 1-y(kbis,1); 
+             if G.Btot(it,i)< (Bmax/120)
+                G.Btrait(it,i) = NaN;
+            end
+        end 
+    end 
+    
+    %Dominant trait values
+    figure(4)
+    clf;
+    set(gcf,'color','w');
+    D.Btrait_y=D.Btrait((years-1)*tf+1:years*tf,:);
+    imAlpha=ones(Imax,tf);
+    imAlpha(isnan(D.Btrait_y'))=0;
+    set(gca,'color',0*[1 1 1]);
+    ie = imagesc(t((years-1)*tf+1:years*tf),log10(xo),D.Btrait_y','AlphaData',imAlpha); 
+    set(gca, 'YDir', 'normal');
+    xlim([(years-1)*tf+1 years*tf]);
+    monthstr = {'J','F','M','A','M','J','J','A','S','O','N','D'};
+    monthnr  = [  1   32  60  91  121 152 182 213 244 274 305 335]+ones(1,12)*tf*(years-1);
+    xticks(monthnr); %x axis : time in months
+    xticklabels(monthstr);
+    futcmap = parula(120);
+    colormap(futcmap)
+    C=colorbar;
+    xlabel('Months')
+    xlabel(C,'\it v (%)');
+    ylabel('Mass \rm [µgC]'); yticks([-7 -6 -5 -4 -3 -2 -1 0 1]); yticklabels(power);
+    set(gca,'FontSize',14);
+    shading interp 
+
+    figure(5)
+    clf;
+    set(gcf,'color','w');
+    G.Btrait_y=G.Btrait((years-1)*tf+1:years*tf,:);
+    imAlpha=ones(Imax,tf);
+    imAlpha(isnan(G.Btrait_y'))=0;
+    set(gca,'color',0*[1 1 1]);
+    ie = imagesc(t((years-1)*tf+1:years*tf),log10(xo),G.Btrait_y','AlphaData',imAlpha);
+    set(gca, 'YDir', 'normal');
+    xlim([(years-1)*tf+1 years*tf]);
+    monthstr = {'J','F','M','A','M','J','J','A','S','O','N','D'};
+    monthnr  = [  1   32  60  91  121 152 182 213 244 274 305 335]+ones(1,12)*tf*(years-1);
+    xticks(monthnr); %x axis : time in months
+    xticklabels(monthstr);
+    C=colorbar;
+    xlabel('Months')
+    xlabel(C,'\it \phi_L');
+    ylabel('Mass \rm [µgC]'); yticks([-7 -6 -5 -4 -3 -2 -1 0 1]); yticklabels(power);
+    set(gca,'FontSize',14);
+    shading interp 
+    
+    %plotting the evolution of the total biomass over a seasonal cycle 
+    figure(6)
+    set(gcf,'color','w');
+    plot(t((years-1)*tf+1:years*tf),D.Btotot((years-1)*tf+1:years*tf),t((years-1)*tf+1:years*tf),G.Btotot((years-1)*tf+1:years*tf),t((years-1)*tf+1:years*tf),G.mixo_tot((years-1)*tf+1:years*tf),'--',t((years-1)*tf+1:years*tf),G.photo_tot((years-1)*tf+1:years*tf),'-.','LineWidth',2); 
+    legend('diatoms','generalists','mixotroph generalists','phototroph generalists');
+    ylabel('Biomass [µgC L^{-1}]');
+    xlim([(years-1)*tf+1 years*tf]);   
+    monthstr = {'J','F','M','A','M','J','J','A','S','O','N','D'};
+    monthnr  = [  1   32  60  91  121 152 182 213 244 274 305 335]+ones(1,12)*tf*(years-1);
+    xticks(monthnr) 
+    xticklabels(monthstr)
+    xlabel('Months')
+    
+    %plotting trophic transfer efficiency over a seasonal cycle
+    figure(15)
+    set(gcf,'color','w');
+    plot(t(1:tf),TTE_2((years-1)*tf+1:years*tf),t(1:365),TTE_3((years-1)*tf+1:years*tf),t(1:365),TTE_np((years-1)*tf+1:years*tf),'LineWidth',2);
+    legend('TTE2','TTE3','TTE not progressive');
+    ylabel('[%]');
+    xlim([1 365]);   
+    monthstr = {'J','F','M','A','M','J','J','A','S','O','N','D'};
+    monthnr  = [  1   32  60  91  121 152 182 213 244 274 305 335];
+    xticks(monthnr) 
+    xticklabels(monthstr)
+    xlabel('Months')
+    
+ 
+%     figure(16)
+%     clf;
+%     set(gcf,'color','w');
+%     plot(Totshannon((years-1)*tf+1:years*tf),TTE_2((years-1)*tf+1:years*tf),Totshannon((years-1)*tf+1:years*tf),TTE_3((years-1)*tf+1:years*tf),Totshannon((years-1)*tf+1:years*tf),TTE_np((years-1)*tf+1:years*tf),'LineWidth',2);
+%     %title('Trophic transfer efficiency as a function of diversity');
+%     legend('TTE2','TTE3','TTE not progressive'); 
+%     xlabel('Diversity \it E(H)');
+%     ylabel('[%]');
+%     xlim([min(Totshannon) max(Totshannon)]);
+    
+    %Trophic transfer efficiency as a function of diversity
+    figure(16)
+    clf;
+    set(gcf,'color','w');
+    subplot('Position',pos1);
+    plot(divphoto((years-1)*tf+1:years*tf),TTE_2((years-1)*tf+1:years*tf),divmixo((years-1)*tf+1:years*tf),TTE_2((years-1)*tf+1:years*tf),'LineWidth',2);
+    legend('mixotrophs diversity','phototrophs diversity')
+    ylabel('Trophic Transfer Efficiency [%]');
+    xlabel('Diversity \it E(H)');
+    
+    subplot('Position',pos2);
+    plot(divmixo((years-1)*tf+1:years*tf),TTE_3((years-1)*tf+1:years*tf),'LineWidth',2);
+    xlabel('Diversity \it E(H)');
+
+    subplot('Position',pos3)
+    plot(divphoto((years-1)*tf+1:years*tf),TTE_np((years-1)*tf+1:years*tf),'LineWidth',2);
+    xlabel('Diversity \it E(H)');
+    
+% Phototrophs and mixotrophs diversity
+%     figure(7)
+%             clf;
+%             set(gcf,'color','w');
+%             plot(t((years-1)*tf+1:years*tf),divphoto((years-1)*tf+1:years*tf),t((years-1)*tf+1:years*tf),divmixo((years-1)*tf+1:years*tf),'LineWidth',2);
+%             legend('photo','mixo'); 
+%             ylabel('\it E(H)','FontSize',16); %title('Evolution of Shannon''s diversity index','FontSize',16)
+%             xlim([(years-1)*tf+1 years*tf]); 
+%             monthstr = {'J','F','M','A','M','J','J','A','S','O','N','D'};
+%             monthnr  = [  1   32  60  91  121 152 182 213 244 274 305 335]+ones(1,12)*tf*(years-1);
+%             xticks(monthnr) 
+%             xticklabels(monthstr)
+%             xlabel('Months');
+
+    
+    %figure(17)
+    % clf;
+% filename = 'prodG.gif';
+% for tt=1:365
+% 
+%      plot(log10(xo),squeeze(G.prod(tt+365,:,:)))
+%      drawnow
+%      frame = getframe(2);
+%      im = frame2im(frame);
+%      [imind,cm] = rgb2ind(im,256);
+%      if tt == 1;
+%            imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
+%      else
+%            imwrite(imind,cm,filename,'gif','WriteMode','append');
+%      end
+%  end 
+
+%Productivity as a function of diversity
+figure(19)
+clf;
+ set(gcf,'color','w');
+ plot(D.Shannon((years-1)*tf+1:years*tf),D.prod((years-1)*tf+1:years*tf),G.Shannon((years-1)*tf+1:years*tf),G.prod((years-1)*tf+1:years*tf),Totshannon((years-1)*tf+1:years*tf),D.prod((years-1)*tf+1:years*tf)+G.prod((years-1)*tf+1:years*tf),'LineWidth',2);
+ legend('diatoms','generalists','all'); 
+ xlabel('Diversity \it E(H)');
+ ylabel('Q [mgC d^{-1} L^{-1}]');
+ xlim([min(D.Shannon) max(Totshannon)]);
+                
+                
+       
+    
+    if strcmp(user_v,'yes')
+        if strcmp(user_w,'Simpson')
+            %Plotting the diversity indexes             
+            figure(7)
+            clf;
+            set(gcf,'color','w');
+            plot(t((years-1)*tf+1:years*tf),D.Simpson((years-1)*tf+1:years*tf),t((years-1)*tf+1:years*tf),G.Simpson((years-1)*tf+1:years*tf),t((years-1)*tf+1:years*tf),Totsimpson((years-1)*tf+1:years*tf),'LineWidth',2);
+            legend('diatoms','generalists','all'); 
+            ylabel('\it D','FontSize',16); %title('Evolution of Simpson''s diversity index','FontSize',16)
+            xlim([(years-1)*tf+1 years*tf]); 
+            monthstr = {'J','F','M','A','M','J','J','A','S','O','N','D'};
+            monthnr  = [  1   32  60  91  121 152 182 213 244 274 305 335]+ones(1,12)*tf*(years-1);
+            xticks(monthnr) 
+            xticklabels(monthstr)
+            xlabel('Months')
+            
+                %Simpson Biodiversity as function of physical forcings
+                figure(8)
+                clf;
+                set(gcf,'color','w');
+                subplot('Position',pos1);
+                plot(L(1:365),D.Simpson((years-1)*tf+1:years*tf),'.',L((years-1)*tf+1:years*tf),G.Simpson((years-1)*tf+1:years*tf),'.',L((years-1)*tf+1:years*tf),Totsimpson((years-1)*tf+1:years*tf),'.');
+                legend('diatoms','generalists','all'); 
+                ylabel('Diversity \it D');
+                xlabel('Light L [µmol photons m^{-2} s^{-1}]');
+                xlim([min(L) max(L)]);
+
+                subplot('Position',pos2);
+                plot(dil((years-1)*tf+1:years*tf),D.Simpson((years-1)*tf+1:years*tf),'.',dil((years-1)*tf+1:years*tf),G.Simpson((years-1)*tf+1:years*tf),'.',dil((years-1)*tf+1:years*tf),Totsimpson((years-1)*tf+1:years*tf),'.');
+                set(gca,'YTick', []);
+                xlabel('Dilution rate [d^{-1}]');
+                xlim([min(dil) max(dil)]);
+
+                subplot('Position',pos3);
+                plot(Z(1:365),D.Simpson((years-1)*tf+1:years*tf),'.',Z(1:365),G.Simpson((years-1)*tf+1:years*tf),'.',Z(1:365),Totsimpson((years-1)*tf+1:years*tf),'.');
+                set(gca,'YTick', []);
+                xlabel('Copepods abundance [µgC L^{-1}]');
+                xlim([min(Z) max(Z)]);
+                
+
+            
+        else %Shannon
+            
+            %Evolution of Shannon''s diversity index
+            figure(7)
+            clf;
+            set(gcf,'color','w');
+            plot(t((years-1)*tf+1:years*tf),D.Shannon((years-1)*tf+1:years*tf),t((years-1)*tf+1:years*tf),G.Shannon((years-1)*tf+1:years*tf),t((years-1)*tf+1:years*tf),Totshannon((years-1)*tf+1:years*tf),'LineWidth',2);
+            legend('diatoms','generalists','all'); 
+            ylabel('\it E(H)','FontSize',16); 
+            xlim([(years-1)*tf+1 years*tf]); 
+            monthstr = {'J','F','M','A','M','J','J','A','S','O','N','D'};
+            monthnr  = [  1   32  60  91  121 152 182 213 244 274 305 335]+ones(1,12)*tf*(years-1);
+            xticks(monthnr) 
+            xticklabels(monthstr)
+            xlabel('Months');
+                
+                %Shannon Biodiversity as function of physical forcings
+                figure(20)
+                clf;
+                set(gcf,'color','w');
+                subplot('Position',pos1);
+                plot(L((years-1)*tf+1:years*tf),D.Shannon((years-1)*tf+1:years*tf),'.',L((years-1)*tf+1:years*tf),G.Shannon((years-1)*tf+1:years*tf),'.',L((years-1)*tf+1:years*tf),Totshannon((years-1)*tf+1:years*tf),'.');
+                legend('diatoms','generalists','all'); 
+                ylabel('Diversity \it E(H)');
+                xlabel('Light L [µmol photons m^{-2} s^{-1}]');
+                xlim([min(L) max(L)]);
+
+                subplot('Position',pos2);
+                plot(dil((years-1)*tf+1:years*tf),D.Shannon((years-1)*tf+1:years*tf),'.',dil((years-1)*tf+1:years*tf),G.Shannon((years-1)*tf+1:years*tf),'.',dil((years-1)*tf+1:years*tf),Totshannon((years-1)*tf+1:years*tf),'.');
+                set(gca,'YTick', []);
+                xlabel('Dilution rate [d^{-1}]');
+                xlim([min(dil) max(dil)]);
+
+                subplot('Position',pos3);
+                plot(Z((years-1)*tf+1:years*tf),D.Shannon((years-1)*tf+1:years*tf),'.',Z((years-1)*tf+1:years*tf),G.Shannon((years-1)*tf+1:years*tf),'.',Z((years-1)*tf+1:years*tf),Totshannon((years-1)*tf+1:years*tf),'.');
+                set(gca,'YTick', []);
+                xlabel('Copepods abundance [µgC L^{-1}]');
+                xlim([min(Z) max(Z)]);
+
+            
+        end
+
+    end 
+end  
